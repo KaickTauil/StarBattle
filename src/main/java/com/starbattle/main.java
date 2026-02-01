22eb7bf65429541971e3530b94e398a5f32a071f
KaickTauil <kaicktauil6@gmail.com>
2026-02-01 20:17:42 -0300

    finalização da primeira versão

--- src/main/java/com/starbattle/main.java
+++ src/main/java/com/starbattle/main.java
@@ -2,7 +2,7 @@ package com.starbattle;
 
 import com.starbattle.controller.GameController;
 
-public class main{
+public class Main{
     public static void main(String[] args) {
         GameController controller = new GameController();
 
